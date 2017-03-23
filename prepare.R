91db55443751678d4c630a98496e34fadc09d5bf
Guy Freeman <git@gfrm.in>
2017-03-23 16:51:45 +0800

    Immediately makesmall after stream_in for each jl file

--- prepare.R
+++ prepare.R
@@ -3,18 +3,17 @@ library(tidyr)
 library(jsonlite)
 library(data.table)
 
-hkexdata <- list()
-for (jsonfileindex in 1:length(list.files(pattern = "*.jl"))) {
-  hkexdata[[jsonfileindex]] <- stream_in(file(list.files(pattern = "*.jl")[jsonfileindex]))
-}
-
 makesmall <- function(bigjl) {
   hkexsmall <- bigjl[,c("corporation", "stock_code", "name", "surname", "othernames", "chinesename", "formtype", "formid", "date", "dateaware", "sharesbefore", "sharesafter", "relevanteventdetails")] %>% flatten %>% filter(formtype %in% c("1", "2", "3A")) %>% gather(key, value, starts_with("shares")) %>% separate(key, into = c("when", "position", "scale"), sep = "\\.") %>% filter(scale == "percent") %>% mutate(value = ifelse(is.na(value), 0, as.numeric(value))) %>% mutate(canonicalname = ifelse(!is.na(name), name, ifelse(!is.na(surname), paste0(surname, ", ", othernames), chinesename)))
   return(hkexsmall)
 }
 
-hkexsmall <- lapply(hkexdata, makesmall)
-rm(hkexdata); gc()
-hkexsmall <- rbindlist(hkexsmall)
+hkexdata <- list()
+for (jsonfileindex in 1:length(list.files(pattern = "*.jl"))) {
+  hkexdatatemp <- stream_in(file(list.files(pattern = "*.jl")[jsonfileindex]))
+  hkexdata[[jsonfileindex]] <- makesmall(hkexdatatemp)
+}
+
+hkexsmall <- rbindlist(hkexdata)
 
 write.csv(hkexsmall, file = "hkexsmall.csv", row.names = FALSE)
