343c27d8c8506f8c37b39d66c086617f05e7b562
Guy Freeman <git@gfrm.in>
2017-04-10 18:32:30 +0800

    Switch to postgresql

--- prepare.R
+++ prepare.R
@@ -4,7 +4,7 @@ library(jsonlite)
 library(data.table)
 
 makesmall <- function(bigjl) {
-  hkexsmall <- bigjl[,c("corporation", "stock_code", "name", "surname", "othernames", "chinesename", "formtype", "formid", "date", "dateaware", "sharesbefore", "sharesafter", "relevanteventdetails")] %>% flatten %>% filter(formtype %in% c("1", "2", "3A")) %>% gather(key, value, starts_with("shares")) %>% separate(key, into = c("when", "position", "scale"), sep = "\\.") %>% filter(scale == "percent") %>% mutate(value = ifelse(is.na(value), 0, as.numeric(value))) %>% mutate(canonicalname = ifelse(!is.na(name), name, ifelse(!is.na(surname), paste0(surname, ", ", othernames), chinesename)))
+  hkexsmall <- bigjl[,c("corporation", "stock_code", "name", "surname", "othernames", "chinesename", "formtype", "formid", "date", "dateaware", "sharesbefore", "sharesafter", "relevanteventdetails")] %>% flatten %>% filter(formtype %in% c("1", "2", "3A")) %>% gather(key, value, starts_with("shares")) %>% separate(key, into = c("beforeafter", "position", "scale"), sep = "\\.") %>% filter(scale == "percent") %>% mutate(value = ifelse(is.na(value), 0, as.numeric(value))) %>% mutate(canonicalname = ifelse(!is.na(name), name, ifelse(!is.na(surname), paste0(surname, ", ", othernames), chinesename)))
   return(hkexsmall)
 }
 
